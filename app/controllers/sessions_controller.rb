c619d5920ec5f4e4d5b09f3db8a4cb30ce7930b3
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-09-23 23:20:09 +0900

    controllerのurlをonly_pathに変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name:session_params[:name])
     if user && user.authenticate(session_params[:password])
       login(user)
-      redirect_to url_for(controller: :user,only_path: false,action: :articles,protocol: :'https')
+      redirect_to url_for(controller: :users,only_path: false,action: :articles,protocol: :'https')
     else
       flash.now[:danger] = 'invalid name or password'
       render new_article_path(protocol: 'https')
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
   end
   def destroy
     logout
-    redirect_to root_path(protocol: 'https')
+    redirect_to url_for(controller: :static_pages,only_path: false,action: :home,protocol: :'https')
   end
 
   def session_params
