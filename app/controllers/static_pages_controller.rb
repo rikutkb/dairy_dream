720bc13731a463a2034904e86a4da811987f0180
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-09-23 22:24:14 +0900

    他のpathもprotocolを指定するように変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if current_user
-      redirect_to user_path(current_user)+'/articles'
+      redirect_to user_path(current_user,protocol: 'https')+'/articles'
     else
       redirect_to url_for(controller: :static_pages,only_path: false,action: :help,protocol: :'https')
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user+'/articles'
+      redirect_to user_path(@user,protocol: 'https')+'/articles'
     else
-      render 'new'
+      render new_user_path(protocol: 'https')
     end
   end
   def show
