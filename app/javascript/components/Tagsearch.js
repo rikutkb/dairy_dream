40cc375d6b0ae7b0aba391b82fde764bf755c7d6
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-10-26 09:30:40 +0900

    tagを multi選択できなかったので修正

--- app/javascript/components/Tagsearch.js
+++ app/javascript/components/Tagsearch.js
@@ -1,6 +1,7 @@
-import React from "react"
+import React,{useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import Select from 'react-select'
+import CreatableSelect from 'react-select/creatable';
 
 class Tagsearch extends React.Component {
   constructor(props){
@@ -8,8 +9,36 @@ class Tagsearch extends React.Component {
     this.state = {
       candidates: []
     }
+
+    this.fetchTags(this.props.kind,"")
+  }
+
+  postTags(kind,keyword){
+    console.log(kind)
+    let post_data = { kind: kind, keyword: keyword };
+    fetch( '/dream_app/tags', {
+      method: 'POST',
+      headers: {
+       'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(post_data)
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(json=>{
+      let candidates = json.map(json=>{
+        return {name:json.name,label:json.name}
+      });
+      this.setState({
+        candidates: candidates
+      });
+    })
+    .catch(e=>{
+    })
   }
   fetchTags(kind,keyword){
+    console.log(kind)
     const params = {
       keyword:keyword,
       kind:kind
@@ -26,20 +55,30 @@ class Tagsearch extends React.Component {
     })
     .then(json=>{
       let candidates = json.map(json=>{
-        return {name:json.name,label:json.name}
+        return {value:json.name,label:json.name}
       });
       this.setState({
         candidates: candidates
       });
+
+      console.log(candidates)
     })
     .catch(e=>{
     })
   }
+
+
   render () {
+
     return (
       <div>
-        <Select options={this.state.candidates}
-        onInputChange={(keyword) => this.fetchTags(this.props.kind,keyword) }
+        <CreatableSelect options={this.state.candidates}
+        isMulti
+        name="tags"
+        onChange={console.log(this.state.candidates)}
+        className="basic-multi-select"
+        classNamePrefix="select"
+        isClearable
         />
       </div>
 
